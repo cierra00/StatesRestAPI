60a1c97913f0b538c26b94d82911bc9116ecffa8
Cierra Ray <cierraray2@gmail.com>
2024-05-08 01:14:37 -0400

    Testing stuff

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -108,34 +108,35 @@ const createNewFunfacts = async(req, res) =>{
 }
 const updateState = async(req, res) =>{
     const code = req.code;
-    let { index, funfact } = req?.body; 
+    let { index, funfact } = req?.body;
+    index -= 1;
 
     if (!index){
         return res.status(400).json({ 
             'message': 'State fun fact index value required' 
         })
     }
+   if (!funfact){
+        return res.status(400).json({ 'message': 'State fun fact value required' })
+    }
+
+    const state = statesData.find(state => state.code === code)
+
+    const savedState = await State.findOne({ stateCode: code }).exec();
+
     if (!savedState?.funfacts?.length){
         return res.status(404).json({ 
             'message': `No Fun Facts found for ${state.state}` 
         })
     }
-    if (!savedState.funfacts[index-1]){
-        return res.status(404).json({ 
-            'message': `No Fun Fact found at that index for ${state.state}`
-         })
-    }    
 
-    if (!funfact){
-        return res.status(400).json({ 
-            'message': 'State fun fact value required'
-         })}
-    
-    const state = statesData.find(state => state.code === code);
-    const dbState = await State.findOne({ stateCode: code }).exec();
-    dbState.funfacts[index-1] = funfact;
-    
-    const result = await dbState.save();
+    if (!savedState.funfacts[index]){
+        return res.status(404).json({ 
+            'message': `No Fun Fact found at that index for ${state.state}` 
+        })
+    }
+    statesDB.funfacts[index] = funfact;   
+    const result = await statesDB.save();   
     res.json(result)
 }
 const deleteState = async(req, res) =>{
